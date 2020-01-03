28906e60226781455503637beacc39652e784d20
kuzma <qzmaaa@gmail.com>
2020-01-03 11:46:25 +0100

    Added controller mock

---
+++ src/main/java/qzma/springframework/didemo/controllers/MyController.java
@@ -0,0 +1,15 @@
+package qzma.springframework.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController
+{
+	public String hello(){
+		System.out.println("hello");
+
+		return "foo";
+	}
+
+
+}
