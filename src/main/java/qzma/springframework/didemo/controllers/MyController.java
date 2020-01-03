e8272303b9af18ea47aba3a299953f8311034f5e
kuzma <qzmaaa@gmail.com>
2020-01-03 15:30:45 +0100

    Added usage of primary annotation

--- src/main/java/qzma/springframework/didemo/controllers/MyController.java
+++ src/main/java/qzma/springframework/didemo/controllers/MyController.java
@@ -2,13 +2,22 @@ package qzma.springframework.didemo.controllers;
 
 import org.springframework.stereotype.Controller;
 
+import qzma.springframework.didemo.services.GreetingService;
+
 @Controller
 public class MyController
 {
+	private GreetingService greetingService;
+
+	public MyController(GreetingService greetingService)
+	{
+		this.greetingService = greetingService;
+	}
+
 	public String hello(){
 		System.out.println("hello");
 
-		return "foo";
+		return greetingService.sayGreeting();
 	}
 
 
--- src/main/java/qzma/springframework/didemo/controllers/PropertyInjectedController.java
+++ src/main/java/qzma/springframework/didemo/controllers/PropertyInjectedController.java
@@ -1,6 +1,7 @@
 package qzma.springframework.didemo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import qzma.springframework.didemo.services.GreetingService;
@@ -9,6 +10,7 @@ import qzma.springframework.didemo.services.GreetingService;
 public class PropertyInjectedController
 {
 	@Autowired
+	@Qualifier("greetingServiceImpl")
 	public GreetingService greetingServiceImpl;
 
 	public String sayHello(){
